6dbceef5cb274fd73e84f80e8d952f77085165e0
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2023-01-19 23:45:06 +0200

    Added models in models.py files

--- fake_csv/models.py
+++ fake_csv/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class UserModel(models.Model):
+    login = models.CharField(max_length=45, verbose_name='Login')
+    password = models.CharField(max_length=45, verbose_name="Password")
+
+
+class NewSchemaModel(models.Model):
+    name_schema = models.CharField(max_length=45, verbose_name='New schema')
+    column_separator = models.CharField(max_length=45, verbose_name='Column separator')
+    string_character = models.CharField(max_length=45, verbose_name='String character')
+    add_column = models.ForeignKey('AddColumnModel', on_delete=models.PROTECT)
+
+
+class AddColumnModel(models.Model):
+    column_name = models.CharField(max_length=45, verbose_name='Column name')
+    column_type = models.CharField(max_length=45, verbose_name='Column type')
+    order = models.PositiveSmallIntegerField(verbose_name='Order')
+    range_int = models.ForeignKey('RangeAgeModel', on_delete=models.PROTECT)
+
+
+class RangeAgeModel(models.Model):
+    range_from = models.PositiveSmallIntegerField(verbose_name='From')
+    range_to = models.PositiveSmallIntegerField(verbose_name='To')
+
+
+class DataSetsModel(models.Model):
+    to_created = models.DateField(auto_now_add=True, verbose_name='Created')
+    num_rows = models.PositiveIntegerField(verbose_name='Rows')
