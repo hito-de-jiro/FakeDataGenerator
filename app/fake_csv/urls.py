c55cec7379306203c00bc2517aad13bd0eb0634e
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2023-02-26 14:05:24 +0200

    Reconnected static files and some changes in templates

--- app/fake_csv/urls.py
+++ app/fake_csv/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 from .views import (
     login,
-    SchemaListView, SchemaCreateView
+    SchemaListView,
+    SchemaCreateView
 )
 
 urlpatterns = [
-    path('', login, name='login'),  # http://127.0.0.1:8000/login/
     path('schemas/', SchemaListView.as_view(template_name="fake_scv/schemamodel_list.html"), name='schema_list'),
     path('add/', SchemaCreateView.as_view(template_name="fake_csv/schema_form.html"), name='add_schemas'),
+    path('', login, name='login/'),  # http://127.0.0.1:8000/login/
 
 ]
 
