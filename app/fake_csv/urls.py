6aa20a858717fd3d90d5333d0cdb4995371f700c
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2023-04-06 11:35:11 +0300

    fix: fixed bad import in fake_csv/urls.py

--- app/fake_csv/urls.py
+++ app/fake_csv/urls.py
@@ -4,7 +4,6 @@ from .views import (
     user_login,
     SchemaListView,
     SchemaCreateView,
-    SchemaUpdateView,
     SchemaDetailView,
     SchemaDeleteView,
     update_schema
@@ -13,7 +12,6 @@ from .views import (
 urlpatterns = [
     path('schemas/', SchemaListView.as_view(), name='schema_list'),  # http://127.0.0.1:8000/schemas/
     path('add/', SchemaCreateView.as_view(), name='add_schemas'),  # http://127.0.0.1:8000/add/
-    # path('schemas/<int:pk>/update', SchemaUpdateView.as_view(), name='schema_edit'),
     path('schemas/<int:pk>/update', update_schema, name='schema_edit'),
     path('schemas/<int:pk>/delete', SchemaDeleteView.as_view(), name='schema_delete'),
     path('schemas/<int:pk>/', SchemaDetailView.as_view(), name='schema_detail'),
