4466c156f0c65f34fa78a8a7c1e947a21dfcb29c
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2023-05-01 20:25:00 +0300

    fix: cosmetic fix

--- app/fake_csv/views.py
+++ app/fake_csv/views.py
@@ -9,7 +9,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
-from django.views import View
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView
 
